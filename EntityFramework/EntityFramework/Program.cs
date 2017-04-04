8e86beb421baefc659be0d1b99efd5652a42661d
shtigran <shakhbekyantigran@gmail.com>
2017-04-04 22:50:53 +0400

    Add some data to DataBase

--- EntityFramework/EntityFramework/Program.cs
+++ EntityFramework/EntityFramework/Program.cs
@@ -13,7 +13,15 @@ namespace EntityFramework
             using (var db = new ModelContainer())
             {
 
+                People man1 = new People();
+                man1.Age = 32;
+                man1.FirstName = "Pol";
+                man1.LastName = "Robson";
 
+                People man2 = new People() { Age = 24, LastName = "Nickson", FirstName = "Jack", Details = new Details() { City = "Yervan", Phone = "077 345 678"}};
+
+                db.PeopleSet.Add(man1);
+                db.PeopleSet.Add(man2);
 
 
             }
